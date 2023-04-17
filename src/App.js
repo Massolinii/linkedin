d88530deb15e78aab6882ea803800d06fa9ca07d
Massolinii <massimilianoesposito548@gmail.com>
2023-04-17 11:44:14 +0200

    Start Point

--- src/App.js
+++ src/App.js
@@ -1,24 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Container, Row, Col } from "react-bootstrap";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Container>
+        <Row>{/*MyNav*/}</Row>
+        <Routes>
+          <Route path="*" />
+          <Route path="/profile" element={<></>} />
+        </Routes>
+        <Row>{/*MyFooter*/}</Row>
+      </Container>
+    </BrowserRouter>
   );
 }
 
