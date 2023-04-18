78059ffd782ac28ee650d9416113b65773c17e83
promi93 <116350522+promi93@users.noreply.github.com>
2023-04-18 15:56:08 +0200

    da sistemare il Modal

--- src/component/profileRoute/Expirience.jsx
+++ src/component/profileRoute/Expirience.jsx
@@ -1,16 +1,61 @@
 import { Container, Row, Col } from "react-bootstrap";
-import { BsPlusLg, BsPencil } from "react-icons/bs";
+import {
+  BsPlusLg,
+  BsPencil,
+  BsBriefcaseFill,
+  BsClockFill,
+} from "react-icons/bs";
+import { Dropdown } from "react-bootstrap";
 import "bootstrap/dist/css/bootstrap.min.css";
+import React, { useState } from "react";
+import AddExperience from "./FormExperience/AddExperience";
 
 const Experience = () => {
+  const [showDropdown, setShowDropdown] = useState(false);
+  const [showModal, setShowModal] = useState(false);
+
+  const handleToggle = () => {
+    setShowDropdown(!showDropdown);
+  };
+
+  const handleModalClose = () => {
+    setShowModal(false);
+  };
+
+  const handleAddPosition = () => {
+    setShowModal(true);
+  };
+
   return (
     <>
       <Container className="mt-4 p-4 border rounded-3 bg-light bg-light">
         <Row>
           <div className="d-flex justify-content-between">
             <h4>Esperienza</h4>
-            <div>
-              <BsPlusLg className="me-4" />
+            <div className="d-flex justify-content-between">
+              <div>
+                <Dropdown
+                  show={showDropdown}
+                  onClose={() => setShowDropdown(false)}
+                >
+                  <Dropdown.Toggle
+                    as={BsPlusLg}
+                    className="me-4"
+                    variant="secondary"
+                    id="dropdown-basic"
+                    onClick={handleToggle}
+                  />
+                  <Dropdown.Menu>
+                    <Dropdown.Item onClick={handleAddPosition}>
+                      <BsBriefcaseFill className="me-2" /> Aggiungi Posizione
+                      Lavorativa
+                    </Dropdown.Item>
+                    <Dropdown.Item>
+                      <BsClockFill className="me-2" /> Aggiungi Pausa Lavorativa
+                    </Dropdown.Item>
+                  </Dropdown.Menu>
+                </Dropdown>
+              </div>
               <BsPencil />
             </div>
           </div>
@@ -27,6 +72,7 @@ const Experience = () => {
           </Col>
         </Row>
       </Container>
+      <AddExperience show={showModal} onClose={handleModalClose} />
     </>
   );
 };
---
+++ src/component/profileRoute/FormExperience/AddExperience.jsx
@@ -0,0 +1,158 @@
+import Button from "react-bootstrap/Button";
+import Form from "react-bootstrap/Form";
+import Modal from "react-bootstrap/Modal";
+import { Row, Col } from "react-bootstrap";
+import React, { useState } from "react";
+
+function AddExperience({ show, onHide }) {
+  const [esperienze, setEsperienze] = useState({
+    startDate: "",
+    endDate: "",
+  });
+
+  const handleChange = (field, value) => {
+    setEsperienze({
+      ...esperienze,
+      [field]: value,
+    });
+  };
+
+  return (
+    <Modal.Dialog show={show} onHide={onHide} size="lg">
+      <Modal.Header closeButton>
+        <Modal.Title>Aggiungi esperienza</Modal.Title>
+      </Modal.Header>
+      <Modal.Body>
+        <Form>
+          <h4>Aggiungi esperienza</h4>
+
+          <p> Indica che è obbligatorio</p>
+
+          {/* INPUT QUALIFICA */}
+          <Form.Group className="mb-3" required>
+            <Form.Label>Qualifica*</Form.Label>
+            <Form.Control
+              type="text"
+              placeholder="Esempio: Retail Sales Manager"
+            />
+          </Form.Group>
+
+          {/* INPUT TIPO DI IMPIEGO */}
+          <Form.Group className="mb-3">
+            <Form.Label></Form.Label>
+            <Form.Control type="email" placeholder="Enter email" />
+            <p>
+              Scopri di più sui
+              <a href=".">tipi di impiego</a>.
+            </p>
+          </Form.Group>
+
+          {/* INPUT NOME AZIENDA */}
+          <Form.Group className="mb-3" required>
+            <Form.Label>Nome azienda*</Form.Label>
+            <Form.Control type="text" placeholder="Esempio: Microsoft" />
+          </Form.Group>
+
+          {/* INPUT LOCALITA */}
+          <Form.Group className="mb-3">
+            <Form.Label>Località</Form.Label>
+            <Form.Control type="text" placeholder="Esempio: Milano, Italia" />
+            <p>Scegli un tipo di località &lpar;es.da remoto&rpar;</p>
+          </Form.Group>
+
+          {/* DATA INIZIO E FINE */}
+          <Row>
+            <Col>
+              <Form.Label>Data di inizio</Form.Label>
+              <Form.Control
+                type="date"
+                placeholder="Data di inizio"
+                value={esperienze.startDate}
+                onChange={(e) => {
+                  handleChange("startDate", e.target.value);
+                }}
+              />
+            </Col>
+            <Col>
+              <Form.Label>Data di fine</Form.Label>
+              <Form.Control
+                type="date"
+                placeholder="Data di fine"
+                value={esperienze.endDate}
+                onChange={(e) => {
+                  handleChange("endDate", e.target.value);
+                }}
+              />
+            </Col>
+          </Row>
+
+          {/* CHECKBOX */}
+          <Form.Group className="mb-3" controlId="formBasicCheckbox">
+            <Form.Check type="checkbox" label="Check me out" />
+            <p>Attualmente ricopro questo ruolo</p>
+          </Form.Group>
+
+          {/* DATA INIZIO E FINE */}
+
+          {/* INPUT SETTORE */}
+          <Form.Group className="mb-3" required>
+            <Form.Label>Settore*</Form.Label>
+            <Form.Control
+              type="text"
+              placeholder="Es.: Commercio al dettaglio"
+            />
+            <p>
+              Scopri di più sulle
+              <a href=".">opzioni relative al settore</a>.
+            </p>
+          </Form.Group>
+
+          {/* INPUT DESCRIZIONE */}
+          <Form.Group className="mb-3">
+            <Form.Label>Descrizione</Form.Label>
+            <Form.Control type="texarea" />
+            We'll never share your email with anyone else.
+          </Form.Group>
+
+          {/* INPUT SOMMARIO DEL PROFILO */}
+          <Form.Group className="mb-3">
+            <Form.Label>Sommario del profilo</Form.Label>
+            <Form.Control type="email" />
+            <p>Compare sotto il tuo nome nella parte superiore del profilo</p>
+          </Form.Group>
+
+          {/* BUTTON ADD COMPETENZE */}
+          <h4>Competenze</h4>
+          <p>
+            Ti consigliamo di aggiungere le 5 competenze più utilizzate in
+            questo ruolo. Appariranno anche nella sezione Competenze.
+          </p>
+          <Button className="bg-light border rounded-5 text-primary">
+            + Aggiungi competenza
+          </Button>
+
+          {/* BUTTON ADD MEDIA */}
+          <h4>Media</h4>
+          <p>
+            Aggiungi o inserisci un link a documenti, foto, siti, presentazioni
+            e video esterni. Scopri di più sui{" "}
+            <a href=".">tipi di file multimediali supportati</a>
+          </p>
+          <Button className="bg-light border rounded-5 text-primary">
+            + Aggiunfi media
+          </Button>
+        </Form>
+      </Modal.Body>
+      <Modal.Footer>
+        <Button variant="secondary" onClick={onHide}>
+          Annulla
+        </Button>
+        <Button variant="primary" type="submit">
+          Salva
+        </Button>
+      </Modal.Footer>
+    </Modal.Dialog>
+  );
+}
+
+export default AddExperience;
