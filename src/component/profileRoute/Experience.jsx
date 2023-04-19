c933acb5fb9778aee580167c8dc427658f4a6e11
Daniele Migani <danymigani@gmail.com>
2023-04-19 10:12:46 +0200

    o

--- src/component/profileRoute/Experience.jsx
+++ src/component/profileRoute/Experience.jsx
@@ -1,1 +1,97 @@
+import { Container, Row, Col, Modal } from "react-bootstrap";
+import {
+  BsPlusLg,
+  BsPencil,
+  BsBriefcaseFill,
+  BsClockFill,
+} from "react-icons/bs";
+import { Dropdown } from "react-bootstrap";
+import "bootstrap/dist/css/bootstrap.min.css";
+import React, { useState } from "react";
+import AddExperience from "./FormExperience/AddExperience";
+import BreakExperience from "./FormExperience/BreakExperience";
 
+const Experience = () => {
+  const [showDropdown, setShowDropdown] = useState(false);
+  const [show, setShow] = useState(false);
+  const [showPause, setShowPause] = useState(false);
+
+  const handleToggle = () => {
+    setShowDropdown(!showDropdown);
+  };
+
+  const handleClose = () => setShow(false);
+  const handleClose2 = () => setShowPause(false);
+  const handleShow = () => setShow(true);
+  const handleShow2 = () => setShowPause(true);
+
+  return (
+    <>
+      <Container className="mt-4 p-4 border rounded-3 bg-light bg-light">
+        <Row>
+          <div className="d-flex justify-content-between">
+            <h4>Esperienza</h4>
+            <div className="d-flex justify-content-between">
+              <Dropdown
+                show={showDropdown}
+                onClose={() => setShowDropdown(false)}
+              >
+                <BsPlusLg
+                  className="d-flex me-4 btnChange"
+                  variant="secondary"
+                  id="dropdown-basic"
+                  onClick={handleToggle}
+                />
+                <Dropdown.Menu>
+                  <Dropdown.Item onClick={handleShow}>
+                    <BsBriefcaseFill className="me-2" /> Aggiungi Posizione
+                    Lavorativa
+                  </Dropdown.Item>
+                  <Dropdown.Item onClick={handleShow2}>
+                    <BsClockFill className="me-2" /> Aggiungi Pausa Lavorativa
+                  </Dropdown.Item>
+                </Dropdown.Menu>
+              </Dropdown>
+              <BsPencil className="btnChange" />
+            </div>
+          </div>
+
+          {/* ADDEXPERIENCE */}
+          <div id="modal">
+            <Modal show={show} onHide={handleClose} size="lg">
+              <Modal.Header closeButton>
+                <Modal.Title>Aggiungi esperienza</Modal.Title>
+              </Modal.Header>
+              <Modal.Body>
+                <AddExperience handleClose={handleClose} />
+              </Modal.Body>
+            </Modal>
+          </div>
+
+          {/* BREAKEXPERIENCE */}
+          <Modal show={showPause} onHide={handleClose2} size="lg">
+            <Modal.Header closeButton>
+              <Modal.Title>Aggiungi pausa lavorativa</Modal.Title>
+            </Modal.Header>
+            <Modal.Body>
+              <BreakExperience handleClose2={handleClose2} />
+            </Modal.Body>
+          </Modal>
+          <Col className="d-flex">
+            <img src="*" alt="LOGO AZIENDA" className="me-2" />
+            <Row>
+              <h5>Nome azienda</h5>
+              <p>Durata</p>
+              <p>Luogo</p>
+              <p>
+                Competenze: <span>qui dentro ci vanno le competenze </span>
+              </p>
+            </Row>
+          </Col>
+        </Row>
+      </Container>
+    </>
+  );
+};
+
+export default Experience;
