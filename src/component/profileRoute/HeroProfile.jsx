0082effb7655e358dfeb0a0eba06aaeabf8371c8
Massolinii <massimilianoesposito548@gmail.com>
2023-04-21 17:05:28 +0200

    Small tweaks

--- src/component/profileRoute/HeroProfile.jsx
+++ src/component/profileRoute/HeroProfile.jsx
@@ -20,12 +20,12 @@ function HeroProfile() {
             <Row style={{ height: "400px" }}>
               <Col className="p-0 m-0">
                 <Col className="profileCover">{/* IMMAGINE COPERTINA */}</Col>
-                <div className="profilePicWrap">
+                <div className="linkprofilePicWrap">
                   <Image
                     src={profile.image}
                     alt="profile-picture"
                     roundedCircle
-                    className="profilePic"
+                    className="linkprofilePic"
                   />
                   {/* IMMAGINE PROFILO */}
                 </div>
